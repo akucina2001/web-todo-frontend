79442c20d67e4e3c9b028a1cd0a1c145aea091ee
andch@DESKTOP-73I8BET.(none) <andchg.10@gmail.com>
2021-06-19 23:16:12 +0700

    tahap 3 done

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+  <h1>Selamat Datang</h1>
+  <div>Berikut daftar kerja kita:</div>
+  <ul>
+    <li v-for="item in todos" :key="item.ID"><button @click="hapus(item.ID)">X</button>{{ item.TODO }}</li>
+  </ul>
+  <input type="text" v-model="myText"/>
+  <button @click="tambahkan">Click Me</button>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default{
+  data : () => {
+    return {
+      todos:[],
+      myText: ''
+    }
+  },
+  mounted: function(){
+    axios
+      .get('http://localhost:3000/todo')  
+      .then(result=>{
+        this.todos = result.data
+      })
+  },
+  methods: {
+    tambah : function(){
+      let newItem = {deskripsi : this.myText}
+      axios
+        .post('http://localhost:3000/todo', newItem)
+        .then(()=>{
+          axios
+            .get('http://localhost:3000/todo')
+            .then(result=>{
+              this.todos = result.data
+              this.myText = ''
+            })
+        })
+    },
+    hapus : function(id){
+      axios.delete(`http://localhost:3000/todo/${id}`)
+      .then(()=>{
+        axios
+          .get('http://localhost:3000/todo')
+          .then(result=>{
+            this.todos = result.data
+          })
+      })
+    }
+  }
+}
+</script>
